d44b4f9b841ed05d7285385588b85b071cd2dedc
unknown <sr.sethu91@gmail.com>
2023-09-05 15:29:41 +0530

    stage changes in remote rep.

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -13,7 +13,9 @@ export const Greet = (props: greetProps) =>{
                props.isLoggedin ? `Welcome to react with typescript.. ${props.name}!!!..You have ${msgCount} unread messages..`
                : `welcome guest`
             }
-            <h2>Using git hub</h2>
+            <h2>Using git hub with commands..</h2>
+
+            <h2>New waay to learning..</h2>
         </div>
     )
 }
